91abc99d351ae440ce2db2b951afa873c42adfab
hinghuynh <hinghuynh@gmail.com>
2014-07-05 20:50:45 -0500

    some icons

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
   # validates :content, presence: true
   belongs_to :survey
+  has_many :answers
   has_many :question_answers
   has_many :survey_answers, through: :question_answers, source: :answer
+
 end
